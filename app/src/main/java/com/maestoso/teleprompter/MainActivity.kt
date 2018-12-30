6bb435043b74999d1733f3332de7d0e529de9a26
Debashis Biswas <debashis.biswas@asu.edu>
2018-12-29 17:30:45 -0700

    Added and implemented paste and clear buttons on MainActivity

--- app/src/main/java/com/maestoso/teleprompter/MainActivity.kt
+++ app/src/main/java/com/maestoso/teleprompter/MainActivity.kt
@@ -1,15 +1,22 @@
 package com.maestoso.teleprompter
 
-import android.content.Intent
+import android.content.*
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.preference.PreferenceManager
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
 import android.widget.Button
 import android.widget.EditText
 
 import kotlinx.android.synthetic.main.activity_main.*
+import android.support.design.widget.Snackbar
+import android.content.ClipData.Item
+import android.content.ClipDescription.MIMETYPE_TEXT_PLAIN
+import android.net.Uri
+import android.support.v7.app.AlertDialog
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,8 +29,7 @@ class MainActivity : AppCompatActivity() {
         val theButton = findViewById<Button>(R.id.start_button)
         theButton?.setOnClickListener {
             val intent = Intent(this, TeleprompterActivity::class.java)
-            val theTextField = findViewById<EditText>(R.id.text_input)
-            val theUserString = theTextField.text.toString()
+            val theUserString = text_input.text.toString()
             intent.putExtra(resources.getString(R.string.user_string_extra_key), theUserString)
             startActivity(intent)
         }
@@ -32,18 +38,51 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
-        menuInflater.inflate( R.menu.menu_main, menu)
+        menuInflater.inflate(R.menu.menu_main, menu)
         return true
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_settings -> {
-            val intent = Intent( this, SettingsActivity::class.java )
-            startActivity( intent )
+            val intent = Intent(this, SettingsActivity::class.java)
+            startActivity(intent)
             true
         }
         else -> {
             super.onOptionsItemSelected(item)
+        }
+    }
+
+
+    // Button onClick functions
+
+    fun onClickPasteFromClipboard(view: View) {
+        val theClipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        var thePasteData = ""
+
+        if (theClipboardManager.hasPrimaryClip() &&
+                theClipboardManager.primaryClipDescription.hasMimeType(MIMETYPE_TEXT_PLAIN)) {
+            val theItem = theClipboardManager.primaryClip.getItemAt(0)
+            thePasteData = theItem.text.toString()
+        }
+
+        if (thePasteData.isNotEmpty()) {
+            text_input.setText(thePasteData)
+        }
+    }
+
+    fun onClickClear(view: View) {
+        val theEditText: EditText = text_input
+        if (theEditText.text.isNotEmpty()) {
+            val theDialog = AlertDialog.Builder(this).create().apply {
+                setTitle("Confirm")
+                setMessage("Are you sure you want to clear the text?")
+                setButton(AlertDialog.BUTTON_POSITIVE, "Yes") { _, _ -> theEditText.setText( "" ) }
+                setButton(AlertDialog.BUTTON_NEGATIVE, "Cancel") { _, _ -> }
+            }
+
+            theDialog.show()
+
         }
     }
 }
